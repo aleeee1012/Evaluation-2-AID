e3ca60fb6dc9857b65f76b121749eccf67e12a58
Aleeee <aqui tu correo de la cuenta de git>
2025-10-18 15:04:35 -0300

    parametroscp

--- FUENTES/ppr.py
+++ FUENTES/ppr.py
@@ -6,16 +6,15 @@ import pandas  as pd
 import numpy   as np
 from utility   import *
 
-
-    
 # Obtain Features by use Entropy    
 def gets_features():
     
-    return(F)    
+    return(F)
+
 # Beginning ...
 def main():
-    
-       
+    x = gets_features()
+
 if __name__ == '__main__':   
 	 main()
 
