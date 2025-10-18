b4f69e205eac58919b271e3fe0373c7eb3b4c61f
Aleeee <aqui tu correo de la cuenta de git>
2025-10-18 15:51:37 -0300

    carga de parametros

--- FUENTES/ppr.py
+++ FUENTES/ppr.py
@@ -6,8 +6,30 @@ import pandas  as pd
 import numpy   as np
 from utility   import *
 
-def load_data():
+# Carga parametros del "Conf_ppr.csv"
+def conf_entropy():
+    config = pd.read_csv("FUENTES/conf_ppr.csv", header=None).values.flatten()
+    opt_code = int(config[0])  # 1 = Dispersión, 2 = Disp-Mejorada, 3 = Permutación, 4 = Perm-Mejorada
 
+    if opt_code == 1:
+        opt = 'dispersion'
+    elif opt_code == 2:
+        opt = 'dispersion-mejorada'
+    elif opt_code == 3:
+        opt = 'permutación'
+    elif opt_code == 4:
+        opt = 'permutación-mejorada'
+    else:
+        raise ValueError(f"conf_ppr.csv: opción de entropía inválida ({opt_code}). Debe ser 1, 2, 3 o 4.")
+    
+    lF = int(config[1]) # Longitud del segmento
+    d = int(config[2]) # Dimensión embebida
+    tau = int(config[3]) # Factor de retardo embebido
+    c = int(config[4]) # Número de clase de Entropía Dispersión
+    Smax = int(config[5]) # Número Máximo de Escalas
+    return opt, lF, d, tau, c, Smax
+
+def load_data():
     df1 = pd.read_csv("G2/class1.csv", header=None).values
     df2 = pd.read_csv("G2/class2.csv", header=None).values
     df3 = pd.read_csv("G2/class3.csv", header=None).values
@@ -28,6 +50,7 @@ def gets_features():
 
 # Beginning ...
 def main():
+    opt, lF, d, tau, c, Smax = conf_entropy()
     data1, data2 , data3, data4 = load_data()
     x = gets_features()
 
